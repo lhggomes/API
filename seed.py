4f8383bd9db46a4a90ea9bf64c8d6ca6376d8722
Lucas Henrique Silva Gomes <lucas.gomes@jobconvo.com>
2021-05-02 11:09:32 -0300

    Function to generate automatic data

--- seed.py
+++ seed.py
@@ -1,12 +1,14 @@
-import os, django
+import django
+import os
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setup.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'API.settings')
 django.setup()
 
+import random
 from faker import Faker
 from validate_docbr import CPF
-import random, datetime
-from school.models import Student, Course, Enrollment
+
+from school.models import Student, Course
 
 
 def creating_students(persons: int):
@@ -14,12 +16,13 @@ def creating_students(persons: int):
     Faker.seed(10)
     for _ in range(persons):
         cpf = CPF()
-        nome = fake.name()
+        name = fake.name()
         rg = "{}{}{}{}".format(random.randrange(10, 99), random.randrange(100, 999), random.randrange(100, 999),
                                random.randrange(0, 9))
         cpf = cpf.generate()
-        data_nascimento = fake.date_between(start_date='-18y', end_date='today')
-        a = Student(nome=nome, rg=rg, cpf=cpf, data_nascimento=data_nascimento)
+        birth_date = fake.date_between(start_date='-18y', end_date='today')
+        print(f'Generating student: {name}')
+        a = Student(name=name, rg=rg, cpf=cpf, birth_date=birth_date, active=True)
         a.save()
 
 
@@ -27,13 +30,14 @@ def creating_courses(courses: int):
     fake = Faker('pt_BR')
     Faker.seed(10)
     for _ in range(courses):
-        codigo_curso = "{}{}-{}".format(random.choice("ABCDEF"), random.randrange(10, 99), random.randrange(1, 9))
-        descs = ['Python Fundamentos', 'Python intermediário', 'Python Avançado', 'Python para Data Science',
-                 'Python/React']
-        descricao = random.choice(descs)
-        descs.remove(descricao)
-        nivel = random.choice("BIA")
-        c = Course(codigo_curso=codigo_curso, descricao=descricao, nivel=nivel)
+        code = "{}{}-{}".format(random.choice("ABCDEF"), random.randrange(10, 99), random.randrange(1, 9))
+        desc = ['Python Fundamentals', 'Python Intermediate', 'Python Advanced', 'Python for  Data Science',
+                'Python/React', 'Java Enterprise Edition', 'Java Swing']
+        description = random.choice(desc)
+        desc.remove(description)
+        level = random.choice("BIA")
+        print(f'Generating course: {description}')
+        c = Course(code=code, description=description, level=level)
         c.save()
 
 
