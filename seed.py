53b54d0b276f96b0bf2c54db470af861c75aa594
Lucas Henrique Silva Gomes <lucas.gomes@jobconvo.com>
2021-05-02 10:51:58 -0300

    Function to generate automatic data

---
+++ seed.py
@@ -0,0 +1,41 @@
+import os, django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setup.settings')
+django.setup()
+
+from faker import Faker
+from validate_docbr import CPF
+import random, datetime
+from school.models import Student, Course, Enrollment
+
+
+def creating_students(persons: int):
+    fake = Faker('pt_BR')
+    Faker.seed(10)
+    for _ in range(persons):
+        cpf = CPF()
+        nome = fake.name()
+        rg = "{}{}{}{}".format(random.randrange(10, 99), random.randrange(100, 999), random.randrange(100, 999),
+                               random.randrange(0, 9))
+        cpf = cpf.generate()
+        data_nascimento = fake.date_between(start_date='-18y', end_date='today')
+        a = Student(nome=nome, rg=rg, cpf=cpf, data_nascimento=data_nascimento)
+        a.save()
+
+
+def creating_courses(courses: int):
+    fake = Faker('pt_BR')
+    Faker.seed(10)
+    for _ in range(courses):
+        codigo_curso = "{}{}-{}".format(random.choice("ABCDEF"), random.randrange(10, 99), random.randrange(1, 9))
+        descs = ['Python Fundamentos', 'Python intermediário', 'Python Avançado', 'Python para Data Science',
+                 'Python/React']
+        descricao = random.choice(descs)
+        descs.remove(descricao)
+        nivel = random.choice("BIA")
+        c = Course(codigo_curso=codigo_curso, descricao=descricao, nivel=nivel)
+        c.save()
+
+
+creating_students(200)
+creating_courses(5)
